54cc232fb467b07ec01d5d85a3d3098bae29291f
Jeffrey Xiao <jeffrey.xiao1998@gmail.com>
2019-05-19 18:28:11 -0400

    pooly: refactor name

--- pooly_ex/lib/pooly/pool_supervisor.ex
+++ pooly_ex/lib/pooly/pool_supervisor.ex
@@ -2,7 +2,7 @@ defmodule Pooly.PoolSupervisor do
   use Supervisor
 
   def start_link(pool_config: pool_config) do
-    Supervisor.start_link(__MODULE__, pool_config, name: :"PoolSupervisor#{pool_config[:name]}")
+    Supervisor.start_link(__MODULE__, pool_config, name: name(pool_config[:name]))
   end
 
   def init(pool_config) do
@@ -13,5 +13,9 @@ defmodule Pooly.PoolSupervisor do
 
     opts = [strategy: :one_for_all]
     Supervisor.init(children, opts)
+  end
+
+  def name(pool_name) do
+    :"PoolSupervisor#{pool_name}"
   end
 end
